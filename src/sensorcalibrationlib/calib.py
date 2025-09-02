3975a8713ff29b044e80f23cf307477abfeb334b
kpetrakis <kostispetrak@gmail.com>
2025-09-02 22:54:15 +0300

    Made CalibAPI more robust, regarding the calibration method. Added checks for each API function using the _method property.

--- src/sensorcalibrationlib/calib.py
+++ src/sensorcalibrationlib/calib.py
@@ -1,51 +1,79 @@
 from __future__ import annotations
 from .methods import CalibMethod, LinearRegression
 import numpy as np
-from typing import Dict, Union
+from typing import Union, Optional
 
 class CalibAPI():
-  def __init__(self, method:CalibMethod):
+  def __init__(self, method: Optional[CalibMethod] = None):
     """
     """
-    if isinstance(method, CalibMethod):
-      self._method = method
-    else:
-      raise TypeError(f"Expected method to be CalibMethod derived object, got {type(method).__name__}")
+    match method:
+      case CalibMethod() | None:
+        self._method = method
+      case _:
+        raise TypeError(f"Expected method to be CalibMethod derived object, got {type(method).__name__}")
 
   @property
-  def method(self) -> CalibMethod:
+  def method(self) -> Optional[CalibMethod]:
     """
     """
     return self._method
 
   @method.setter
-  def method(self, method) -> None:
-    self._method = method
+  def method(self, method: Optional[CalibMethod] = None) -> None:
+    match method:
+      case CalibMethod() | None:
+        self._method = method
+      case _:
+        raise TypeError(f"Expected method to be CalibMethod derived object, got {type(method).__name__}")
   
   def calibrate(self, x, y):
     """
+    @TODO: Why I use return res ??
     """
-    print("(CalibAPI::calibrate) called")
-    res = self._method.fit(x, y)
-    return res
+    match self._method:
+      case CalibMethod():
+        print("(CalibAPI::calibrate) called")
+        self._method.fit(x, y)
+        # res = self._method.fit(x, y)
+        # return res
+      case None:
+        raise ValueError("Can't calibrate without a calibration method.")
 
   def predict(self, raw_val) -> Union[int, float, list, tuple, np.ndarray]:
     """ 
     calculate the true values using calibration method parameters from a raw value
     """
-    return self._method(raw_val)
+    match self._method:
+      case CalibMethod():
+        return self._method(raw_val)
+      case None:
+        raise ValueError("Can't predict without a calibration method.")
 
   def parameters(self):
     """
     Method to receive the calibration parameters.
     """
-    return self._method.params()
+    match self._method:
+      case CalibMethod():
+        return self._method.params()
+      case None:
+        raise ValueError("No calibration method is set to get parameters from.")
 
   def export_params(self, filepath: str):
-    self._method.export_params(filepath)
+    match self._method:
+      case CalibMethod():
+        self._method.export_params(filepath)
+      case None:
+        raise ValueError("No calibration method is set to export parameters from.")
+
 
   def import_params(self, filepath: str):
-    self._method.import_params(filepath)
+    match self._method:
+      case CalibMethod():
+        self._method.import_params(filepath)
+      case None:
+        raise ValueError("No calibration method is set to import parameters into.")
 
   # def import_params(self, params_d):
   #   self._method.import_params(params_d)
